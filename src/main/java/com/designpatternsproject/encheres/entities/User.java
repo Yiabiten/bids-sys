e04b4cd5d7c5317ef287a883e7b04f95b0932c69
Yabiten <iabiten@gmail.com>
2015-01-17 01:37:32 +0000

    entities done

--- src/main/java/com/designpatternsproject/encheres/entities/User.java
+++ src/main/java/com/designpatternsproject/encheres/entities/User.java
@@ -1,5 +1,70 @@
 package com.designpatternsproject.encheres.entities;
 
+import java.util.Collection;
+
 public class User {
+	private Long idUser;
+	private String firstName;
+	private String lastName;
+	private String phone;
+	private String address;
+	private String username;
+	public Long getIdUser() {
+		return idUser;
+	}
+	public void setIdUser(Long idUser) {
+		this.idUser = idUser;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getProfession() {
+		return profession;
+	}
+	public void setProfession(String profession) {
+		this.profession = profession;
+	}
+	public Collection<Card> getCards() {
+		return cards;
+	}
+	public void setCards(Collection<Card> cards) {
+		this.cards = cards;
+	}
+	private String password;
+	private String profession;
+	Collection<Card> cards;
 
 }
